b79f803dc060f54abb665b60d3c493af1b322512
Ignas Maslinskas <ignasmaslinskas@gmail.com>
2018-05-31 18:06:39 +0300

    Fix error

--- source/ext.js
+++ source/ext.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-    updatePage();
+  $('#blocker_downl').remove();
+  updatePage();
 });
 
 //Functions for funciontality (thats sounds good)
 function getSongInfo(uid) {
-  $.post( "https://www.pakartot.lt/api/backend/frontend/player/play.php", { type: "tid", id: uid})
-    .done(function(data) {
+  $.post("https://www.pakartot.lt/api/backend/frontend/player/play.php", { type: "tid", id: uid })
+    .done(function (data) {
       json = JSON.parse(data);
       var fileUrl = json['info']['filename'];
       var songName = json['info']['artist'].substr(1, json['info']['artist'].length) + " - " + json['info']['title'] + ".mp3";
@@ -19,30 +20,30 @@ function downloadSong(url, name) {
   totalDownloads = totalDownloads + 1;
   updateDownloadToast();
   fetch(url)
-  .then(function(response) {
-    console.log(name);
-    return response.blob();
-  })
-  .then(function(myBlob) {
-    console.log(name);
-    saveAs(myBlob, name);
-    currentDownloaded = currentDownloaded + 1;
-    updateDownloadToast();
-  });
+    .then(function (response) {
+      console.log(name);
+      return response.blob();
+    })
+    .then(function (myBlob) {
+      console.log(name);
+      saveAs(myBlob, name);
+      currentDownloaded = currentDownloaded + 1;
+      updateDownloadToast();
+    });
 }
 
 // Notifications toast
 function downloadStartNotification(song) {
   $.toast({
-    text : "Pradeta siųsti daina " + song + ".",
-    showHideTransition : "slide",
-    bgColor : "#2f2e2e",
-    loader : false,
-    textColor : "white",
-    hideAfter : 3000,
-    stack : 5,
-    textAlign : "center",
-    position : "bottom-right"
+    text: "Pradeta siųsti daina " + song + ".",
+    showHideTransition: "slide",
+    bgColor: "#2f2e2e",
+    loader: false,
+    textColor: "white",
+    hideAfter: 3000,
+    stack: 5,
+    textAlign: "center",
+    position: "bottom-right"
   });
 }
 
@@ -54,23 +55,23 @@ var downloadToast = undefined;
 function updateDownloadToast() {
   if (typeof downloadToast == 'undefined') {
     downloadToast = $.toast({
-      text : "<div class='loading' style='display: inline-flex;'></div><p style='font-size: 15px; padding-top: 5px;'>Atsiųstos " + currentDownloaded + "/" + totalDownloads + " dainos.</p>",
-      showHideTransition : "slide",
-      loader : false,
-      bgColor : "#2f2e2e",
-      textColor : "white",
-      hideAfter : "false",
-      textAlign : "center",
-      position : "bottom-left"
+      text: "<div class='loading' style='display: inline-flex;'></div><p style='font-size: 15px; padding-top: 5px;'>Atsiųstos " + currentDownloaded + "/" + totalDownloads + " dainos.</p>",
+      showHideTransition: "slide",
+      loader: false,
+      bgColor: "#2f2e2e",
+      textColor: "white",
+      hideAfter: "false",
+      textAlign: "center",
+      position: "bottom-left"
     });
   } else if (totalDownloads != currentDownloaded) {
     downloadToast.update({
-      text : "<div class='loading' style='display: inline-flex;'></div><p style='font-size: 15px; padding-top: 5px;'>Atsiųstos " + currentDownloaded + "/" + totalDownloads + " dainos.</p>"
+      text: "<div class='loading' style='display: inline-flex;'></div><p style='font-size: 15px; padding-top: 5px;'>Atsiųstos " + currentDownloaded + "/" + totalDownloads + " dainos.</p>"
     });
   } else if (totalDownloads == currentDownloaded) {
     downloadToast.update({
-      text : "<div class='loading' style='display: inline-flex;'></div><p style='font-size: 15px; padding-top: 5px;'>Atsiųstos " + currentDownloaded + "/" + totalDownloads + " dainos.</p>",
-      hideAfter : 5000
+      text: "<div class='loading' style='display: inline-flex;'></div><p style='font-size: 15px; padding-top: 5px;'>Atsiųstos " + currentDownloaded + "/" + totalDownloads + " dainos.</p>",
+      hideAfter: 5000
     });
     downloadToast = undefined;
     totalDownloads = 0;
@@ -80,7 +81,7 @@ function updateDownloadToast() {
 
 // Listening for URL changes to update page.
 function updatePage() {
-  $(".play").each(function(data) {
+  $(".play").each(function (data) {
     if ($(this).find("button.d_button").length == 0) {
       trackid = $(this).children("a").data("id");
       if (trackid) {
@@ -102,12 +103,12 @@ var clickHandler = function () {
 }
 
 // Monitoring URL changes.
-var oldURL= ""
+var oldURL = ""
 var urlChangeHandler = window.setInterval(checkURLChange, 200)
-function checkURLChange(){
+function checkURLChange() {
   newURL = document.URL;
-  if(newURL !== oldURL){
-   updatePage();
-   oldURL = newURL;
+  if (newURL !== oldURL) {
+    updatePage();
+    oldURL = newURL;
   }
 }
